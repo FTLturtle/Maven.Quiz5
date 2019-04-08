c45e7bf73e4798e13d1f40d720dda676541b3c84
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-04-08 09:41:00 -0400

    finished palindrome evaluator

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,21 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        return Arrays.stream(StringEvaluator.getAllSubstrings(string)).filter(PalindromeEvaluator::isPalindrome).toArray(String[]::new);
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder(string);
+        return stringBuilder.reverse().toString();
     }
 }
